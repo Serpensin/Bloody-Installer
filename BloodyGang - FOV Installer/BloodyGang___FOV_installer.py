c4d4b3d8045c7e0411c1fd15ad2ee1a20582b667
Serpensin <wissmann.matti@gmail.com>
2021-07-03 19:17:51 +0200

    • Fixed a bug that occurred when DBD was moved from C-Drive.

--- BloodyGang - FOV Installer/BloodyGang___FOV_installer.py
+++ BloodyGang - FOV Installer/BloodyGang___FOV_installer.py
@@ -16,12 +16,12 @@ root = Tk()
 root.withdraw()
 discord = "https://discord.gg/xk9kJnVxTh"
 fovlinkLoad = requests.get('https://bloodygang.serveblog.net/index.php/s/N4jQosCBL5SPWfi/download')
-dbd = ("C:\Program Files (x86)\Steam\steamapps\common\Dead by Daylight")
-
+dbd_folder = ("C:\Program Files (x86)\Steam\steamapps\common\Dead by Daylight")
+dbd_exe = ("C:\Program Files (x86)\Steam\steamapps\common\Dead by Daylight\DeadByDaylight.exe")
 
 #Determines the location where DeadByDaylight is curently installed. (Working)
 def selectGame():
-    if not os.path.exists(dbd):
+    if not os.path.exists(dbd_exe):
         root.iconbitmap("C:/Users/wissm/OneDrive/Bilder/ICO/BG.ico")
         root.filename = filedialog.askopenfilename(initialdir="C:\\", title="Select 'DeadByDaylight.exe' from your Gamefolder.", filetypes=[("DeadByDaylight .exe")])
         if "DeadByDaylight.exe" in root.filename:
@@ -57,8 +57,8 @@ def fovMain():
 
 #SSL (Working)
 def pak():
-    if os.path.exists(dbd):
-        shutil.copy2('BG\pakchunk1-WindowsNoEditor.pak', os.path.join(dbd, "DeadByDaylight\Content\Paks"))
+    if os.path.exists(dbd_exe):
+        shutil.copy2('BG\pakchunk1-WindowsNoEditor.pak', os.path.join(dbd_folder, "DeadByDaylight\Content\Paks"))
     else:
         shutil.copy2('BG\pakchunk1-WindowsNoEditor.pak', os.path.join((os.path.dirname(root.filename)), "DeadByDaylight\Content\Paks"))
     
